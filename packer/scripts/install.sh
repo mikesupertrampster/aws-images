7bce924a59b0144dbe74c3431d5a52a2b7eebe9a
Michael Liu <mikesupertrampster@gmail.com>
2019-04-22 15:52:26 +0100

    Try to delay the install of 'software-properties-common'.

--- packer/scripts/install.sh
+++ packer/scripts/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -eux
 
 apt-get -qq update
-apt-get -y -qq upgrade
-apt-get -y -qq install software-properties-common
 apt-get -y -qq install apt-utils
+apt-get -y -qq upgrade
+sleep 5; apt-get -y -qq install software-properties-common
 
 apt-add-repository ppa:ansible/ansible
 apt-get -qq update
