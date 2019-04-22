8060be799f17311c489d0f47052b9806f738aea8
Michael Liu <mikesupertrampster@gmail.com>
2019-04-22 15:22:05 +0100

    Rearrange apt-get commands.

--- packer/scripts/install.sh
+++ packer/scripts/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -eux
 
 apt-get update
-apt-get  -y install apt-utils software-properties-common
+apt-get -y upgrade
+apt-get -y install software-properties-common
+apt-get -y install apt-utils
 
 apt-add-repository ppa:ansible/ansible
 apt-get update
