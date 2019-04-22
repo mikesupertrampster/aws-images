2b5be18bb9327e0d7502620d1e77d3a69c290110
Michael Liu <mikesupertrampster@gmail.com>
2019-04-22 16:04:01 +0100

    Silence apt commands.

--- packer/scripts/install.sh
+++ packer/scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -eux
 
-apt-get -qq update
-apt-get -y -qq install apt-utils
-apt-get -y -qq upgrade
-sleep 5; apt-get -y -qq install software-properties-common
+apt-get -qq update -qq
+apt-get -y -qq install -qq apt-utils
+apt-get -y -qq upgrade -qq
+sleep 5; apt-get -y -qq install -qq software-properties-common
 
 apt-add-repository ppa:ansible/ansible
-apt-get -qq update
-apt-get -y -qq install ansible
+apt-get -qq update -qq
+apt-get -y -qq install -qq ansible
