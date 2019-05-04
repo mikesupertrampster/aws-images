2f315f69976599f8517972c129c93521fb97a907
Michael Liu <mikesupertrampster@gmail.com>
2019-05-04 10:48:52 +0100

    Tweak vault image.

--- ansible/roles/vault/files/certbot.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source /etc/environment
-
-certbot certonly --agree-tos --non-interactive -d "${VAULT_FQDN}" \
-                 --config-dir=/etc/vault/letsencrypt/config \
-                 --work-dir=/etc/vault/letsencrypt/work \
-                 --logs-dir=/etc/vault/letsencrypt/logs \
-                 --dns-route53 -m ${CERT_RENEW_CONTACT_EMAIL:-abc@xyz.123} \
-                 --server ${LETSENCRYPT_API}
